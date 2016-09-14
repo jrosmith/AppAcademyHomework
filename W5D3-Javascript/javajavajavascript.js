bde715b96bf5fe574ca644841c7e0097ff86d713
Joshua Smith <smithjoshr@gmail.com>
2016-09-14 10:09:42 -0400

    Fix isPrime method

--- W5D3-Javascript/javajavajavascript.js
+++ W5D3-Javascript/javajavajavascript.js
@@ -2,8 +2,8 @@ function madLib(verb, adjective, noun){
   return `We shall ${verb} the ${adjective} ${noun}`;
 }
 
-console.log(madLib('fly', 'iridescent', 'zoo'));
-console.log(madLib('eat', 'rolling', 'cactus'));
+// console.log(madLib('fly', 'iridescent', 'zoo'));
+// console.log(madLib('eat', 'rolling', 'cactus'));
 
 
 function isOdd(num){
@@ -14,23 +14,23 @@ function isOdd(num){
   return bool;
 }
 
-console.log(isOdd(2) === false);
-console.log(isOdd(5) === true);
-console.log(isOdd(-17) === true);
+// console.log(isOdd(2) === false);
+// console.log(isOdd(5) === true);
+// console.log(isOdd(-17) === true);
 
 function yell(str){
   return str.toUpperCase();
 }
 
-console.log(yell('i am yelling') === 'I AM YELLING');
+// console.log(yell('i am yelling') === 'I AM YELLING');
 
 function isSubstring(searchString, substring){
   return(searchString.indexOf(substring) !== -1);
 }
 
-console.log(isSubstring("The cat went to the store", "he cat went") === true);
-console.log(isSubstring("time to program", "time") === true);
-console.log(isSubstring("Jump for joy", "joys") === false);
+// console.log(isSubstring("The cat went to the store", "he cat went"));
+// console.log(isSubstring("time to program", "time") === true);
+// console.log(isSubstring("Jump for joy", "joys") === false);
 
 function xOR(val1, val2){
   return (val1 || val2) && !(val1 && val2);
@@ -51,22 +51,22 @@ function isPrime(num){
     return true;
   } else {
 
-    let i = 0;
+    let i = 2;
 
     while (i < Math.sqrt(num)) {
       if(num % i === 0){
-        i ++;
-        continue;
-      } else {
         return false;
+      } else {
+        i ++;
       }
     }
   }
   return true;
 }
 
-console.log(isPrime(2) === true);
+// console.log(isPrime(2) === true);
 console.log(isPrime(10) === false);
+console.log(isPrime(13) === true);
 console.log(isPrime(15485863) === true);
 console.log(isPrime(3548563) === false);
 
@@ -77,20 +77,20 @@ function printChildren(parent, ...children){
   });
 }
 
-printChildren("George", "Cassie", "Jeff", "Roger");
-console.log("Roger died of cholera");
-printChildren("George", "Cassie", "Jeff");
+// printChildren("George", "Cassie", "Jeff", "Roger");
+// console.log("Roger died of cholera");
+// printChildren("George", "Cassie", "Jeff");
 
 function addThree (a, b, c) {
   return a + b + c;
 }
 var arr = [1,2,3];
 
-console.log(addThree(...arr));
+// console.log(addThree(...arr));
 
 function dinnerTonightIs(food = 'pizza'){
   console.log(`Dinner tonight is ${food}`);
 }
 
-dinnerTonightIs('some inferior food');
-dinnerTonightIs();
+// dinnerTonightIs('some inferior food');
+// dinnerTonightIs();
